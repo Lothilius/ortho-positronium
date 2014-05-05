cee11a3dda9435736616eff13e8208f708827f71
Lothilius <mars110110@aol.com>
2014-05-05 01:38:50 -0500

    Include normalizing function and configuration for last use.

--- monty.py
+++ monty.py
@@ -16,6 +16,20 @@ def arrayFromFile(filename):
             dataArray.append(row)
     return dataArray
 
+def normlize(data_small, data_large, binnum):
+    """Normalizes the data of the larger count data_large
+    :rtype : numpy array
+    according to the count rate of data_small"""
+
+    #data_small = np.array(data_small)
+    #data_large = np.array(data_large)
+
+    percent = data_small[binnum] / float(data_large[binnum])
+    finlist = []
+    for each in data_large:
+        num = round(each * percent, 0)
+        finlist.append(int(num))
+    return finlist
 
 #Extract data from the list made from reading the file
 def cleanData(data):
@@ -47,24 +61,60 @@ def plotevents(datalist):
 def combine(run1, run2, run3, inputFileDer):
     """Combines the two sets of data for the files entered
     at the beginning of the program"""
+    binnum = 221
     data1 = arrayFromFile(inputFileDer + run1)
-    data1 = cleanData(data1)
-
     data2 = arrayFromFile(inputFileDer + run2)
+
+
+    data1 = cleanData(data1)
     data2 = cleanData(data2)
+    #Normalize data according to the lower vallue in bin number binnum
+    if data1[binnum] < data2[binnum]:
+        data2 = normlize(data1, data2, binnum)
+    else:
+        data1 = normlize(data2, data1, binnum)
 
-    prob1 = getprob(data1)
-    prob2 = getprob(data2)
-
-    if run3 is not 0:
+    if run3 != 0:
         data3 = arrayFromFile(inputFileDer + run3)
         data3 = cleanData(data3)
 
+        if data1[binnum] < data3[binnum]:
+            data3 = normlize(data1, data3, binnum)
+        else:
+            data1 = normlize(data3, data1, binnum)
+            data2 = normlize(data3, data2, binnum)
+
+        prob1 = getprob(data1)
+        prob2 = getprob(data2)
         prob3 = getprob(data3)
+    else:
+        prob1 = getprob(data1)
+        prob2 = getprob(data2)
 
-    createcounts(prob1, prob2, prob3)
+    bincount = []
+    for each in range(1, 100):
+        simcounts = createcounts(prob1, prob2, prob3)
+        bincount.append(simcounts[221])
+        if each == 1:
+            filename = inputFileDer + 'simulationS'
+            arrayTofile(simcounts, filename)
 
-#Create a range of randum numbers.
+
+    filename = inputFileDer + "countS"
+    arrayTofile(bincount, filename)
+
+#write an array to a file.
+def arrayTofile(dataArray, fileNum):
+    fileName = fileNum + "_a.csv"
+    print(fileName)
+    with open(fileName, 'w+') as csvfile:
+        linewriter = csv.writer(csvfile, delimiter= ",")
+        for each in dataArray:
+            linewriter.writerow([each])
+    print("done")
+
+
+#Create2jksrange of randum numbers.
 def rndnumbers(binrng, totcounts):
     numbers = []
     for i in range(0, totcounts):
@@ -73,16 +123,15 @@ def rndnumbers(binrng, totcounts):
 
 #Given lists of counts for each bin spit out the coincidence plot
 def createcounts(prob1, prob2, prob3):
-    coin = [0] * len(prob1)
+    coin = [0] * 512
     counts1 = []
     counts2 = []
     counts3 = []
 
     #Create three lists of 10000 random numbers
-    number1 = rndnumbers(len(prob1), 10000000)
-    number2 = rndnumbers(len(prob2), 10000000)
-    number3 = rndnumbers(len(prob3), 10000000)
-    print(len(prob1), len(prob2), len(prob3))
+    number1 = rndnumbers(len(prob1), 500027)
+    number2 = rndnumbers(len(prob2), 500027)
+    number3 = rndnumbers(len(prob3), 500027)
 
     for item in number1:
         binnum = prob1[item]
@@ -97,16 +146,16 @@ def createcounts(prob1, prob2, prob3):
         counts3.append(binnum)
 
 
-    for i in range(len(counts3)):
+    for i in range(len(counts1)):
         binnum = counts1[i] + counts2[i] + counts3[i]
         if binnum < len(coin):
             coin[binnum] += 1
 
-    print(len(coin))
+    return(coin)
 
     #for item in binnum:
 
-    plotevents(coin)
+
 
 #Create a probability array with the number of events in a bin is the number of occurances in the array for the bin.
 #Items in array are bin numbers.
@@ -123,16 +172,24 @@ def getprob(data):
 
 
 def main():
-    file1 = "ACal_2.Spe" #raw_input("Type in file name: ")
-    file2 = "BCal_2.Spe" #raw_input("Type in file name: ")
-    file3 = "CCal_2.Spe"
+    #Set apropriate directory depending on where I am working from.
+    for file in os.listdir("/Users/"):
+        if file == 'martin':
+            print("Welcome Martin")
+            inputFileDer = "/Users/martin/Dropbox/School/Spring-2014/PHY-474/Labs/Positronium/data/Enclosed/HV_Failure/"
+            break
+        elif file == 'admin':
+            print("Welcome Admin")
+            inputFileDer = "/Users/admin/Dropbox/School/Spring-2014/PHY-474/Labs/Positronium/data/Enclosed/HV_Failure/"
+            break
 
-    combine(file1, file2, file3)
+    file1 = "ACal_5.Spe" #raw_input("Type in file name: ")
+    file2 = "BCal_5.Spe" #raw_input("Type in file name: ")
+    file3 = "CCal_5.Spe"
 
-    #plotevents(data)
+    combine(file1, file2, file3, inputFileDer)
 
     return "done"
-
 
 
 main()
